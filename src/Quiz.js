b9af9e1a3fb165372454797a9b6cfa03219509ed
pratibhabinamdar <130791814+pratibhabinamdar@users.noreply.github.com>
2023-04-21 01:41:04 +0530

    6 files modified, 7 added, 5 deleted

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
-let quizData = require('./quiz_data.json');
+import React, { Component } from './react';
 import QuizQuestion from './QuizQuestion.js';
+import QuizEnd from './QuizEnd.js';
 
-class Quiz extends React.Component {
-  render() {
-    const quizData = require('./quiz_data.json');
-    const quiz_question = quizData.quiz_questions[this.state.quiz_position - 1];
-    return (
-      <div className="QuizQuestion">
-        <QuizQuestion quiz_question={quiz_question} />
-      </div>
-    );
-  }
+let quizData = require('./quiz_data.json');
 
+class Quiz extends Component {
   constructor(props) {
     super(props);
     this.state = { quiz_position: 1 };
   }
+
+  render() {
+    const isQuizEnd =
+      this.state.quiz_position - 1 === quizData.quiz_questions.length;
+
+    return (
+      <div>
+        {isQuizEnd ? (
+          <QuizEnd />
+        ) : (
+          <QuizQuestion
+            quiz_question={
+              quizData.quiz_questions[this.state.quiz_position - 1]
+            }
+          />
+        )}
+      </div>
+    );
+  }
 }
+
 export default Quiz;
