fddfa6bd065f52ad86018f0c829377ce3142bef4
pratibhabinamdar <130791814+pratibhabinamdar@users.noreply.github.com>
2023-04-21 02:01:33 +0530

    1 file modified

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,21 +1,15 @@
-import React, { Component } from './react';
-import QuizQuestion from './QuizQuestion.js';
-import QuizEnd from './QuizEnd.js';
-
-let quizData = require('./quiz_data.json');
+import React, { Component } from 'react';
+import QuizQuestion from './QuizQuestion';
+import QuizEnd from './QuizEnd';
+import quizData from './quiz_data.json';
 
 class Quiz extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { quiz_position: 1 };
-  }
-
   render() {
     const isQuizEnd =
       this.state.quiz_position - 1 === quizData.quiz_questions.length;
 
     return (
-      <div>
+      <div className="Quiz">
         {isQuizEnd ? (
           <QuizEnd />
         ) : (
@@ -27,6 +21,11 @@ class Quiz extends Component {
         )}
       </div>
     );
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = { quiz_position: 1 };
   }
 }
 
