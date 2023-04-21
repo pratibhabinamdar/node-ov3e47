e6004fca744cf10ec20f412076657caae8beb7d5
pratibhabinamdar <130791814+pratibhabinamdar@users.noreply.github.com>
2023-04-21 13:14:13 +0530

    4 files modified

--- src/Quiz.js
+++ src/Quiz.js
@@ -4,28 +4,27 @@ import QuizEnd from './QuizEnd';
 import quizData from './quiz_data.json';
 
 class Quiz extends Component {
-  render() {
-    const isQuizEnd =
-      this.state.quiz_position - 1 === quizData.quiz_questions.length;
-
-    return (
-      <div className="Quiz">
-        {isQuizEnd ? (
-          <QuizEnd />
-        ) : (
-          <QuizQuestion
-            quiz_question={
-              quizData.quiz_questions[this.state.quiz_position - 1]
-            }
-          />
-        )}
-      </div>
-    );
-  }
-
   constructor(props) {
     super(props);
-    this.state = { quiz_position: 1 };
+    this.state = {
+      quiz_position: 0,
+    };
+  }
+  showNextQuestion() {
+    this.setState((state) => ({
+      quiz_position: state.quiz_position + 1,
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <QuizQuestion
+          quiz_question={quizData.quiz_questions[this.state.quiz_position]}
+          showNextQuestionHandler={this.showNextQuestion.bind(this)}
+        />
+      </div>
+    );
   }
 }
 
